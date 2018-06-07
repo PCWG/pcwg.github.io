1735875b8c9256563554aab61c8f4be7406a1ea8
Peter Stuart <peter.stuart@res-group.com>
2018-06-07 23:02:45 +0100

    migration of old website content

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.module('app', [])
+.directive('scrollTo', function ($location, $anchorScroll) {
+  return function(scope, element, attrs) {
+
+    element.bind('click', function(event) {
+        event.stopPropagation();
+        var off = scope.$on('$locationChangeStart', function(ev) {
+            off();
+            ev.preventDefault();
+        });
+        var location = attrs.scrollTo;
+        $location.hash(location);
+        $anchorScroll();
+    });
+
+  };
+});
